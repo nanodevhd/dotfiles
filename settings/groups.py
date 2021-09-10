5e9c51531e3c85e879652437e98e7f55b9aa488b
nanodevhd <76785296+nanodevhd@users.noreply.github.com>
2021-09-10 18:02:58 +0100

    v1

---
+++ settings/groups.py
@@ -0,0 +1,40 @@
+# Antonio Sarosi
+# https://youtube.com/c/antoniosarosi
+# https://github.com/antoniosarosi/dotfiles
+
+# Qtile workspaces
+
+from libqtile.config import Key, Group
+from libqtile.command import lazy
+from .keys import mod, keys
+
+
+# Get the icons at https://www.nerdfonts.com/cheat-sheet (you need a Nerd Font)
+# Icons: 
+# nf-fa-firefox, 
+# nf-fae-python, 
+# nf-dev-terminal, 
+# nf-fa-code, 
+# nf-oct-git_merge, 
+# nf-linux-docker,
+# nf-mdi-image, 
+# nf-mdi-layers
+
+# groups = [Group(i) for i in [
+#     "   ", "   ", "   ", "   ", "  ", "   ", "   ", "   ", "   ",
+# ]]
+
+groups = [Group(i) for i in [
+    "   ", "   ", "   ", "   ", "   ",  "   ", 
+]]
+
+groupKeys = ["a", "z", "e", "d", "c", "x", "v"]
+
+for i, group in enumerate(groups):
+    actual_key = groupKeys[i]
+    keys.extend([
+        # Switch to workspace N
+        Key([mod], actual_key, lazy.group[group.name].toscreen()),
+        # Send window to workspace N
+        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
+    ])
